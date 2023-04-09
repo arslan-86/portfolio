544e1fe46af0a0766b971985e92e45116c9a2a3c
Arslan <arslankhan863345@gmail.com>
2023-04-08 17:21:12 -0700

    added Smtp js and email confirmation box

--- script.js
+++ script.js
@@ -2,4 +2,55 @@
 // font-family: 'Poppins', sans-serif; 300 400 500 
 // font-family: 'Roboto', sans-serif; 300 400 500
 
+const senderName = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const submitBtn = document.getElementById('submit_btn');
+
+const alertModal = document.getElementById('alert_modal');
+const alertTitle = document.getElementById('alert_title');
+const alertBody = document.getElementById('alert_body');
+const alertCloseBtn = document.getElementById('alert_close');
+const alertBox = document.getElementById('alert_box');
+
+
+
+
+submitBtn.addEventListener('click', (e) => {
+   e.preventDefault();
+   // alertModal.style.display = 'block';
+   const body = `Name: ${senderName.value} 
+    Email: ${email.value}
+    Message: ${message.value} `;
+
+   Email.send({
+      SecureToken: "9c6b9d25-d4b5-4a3c-8b17-3c45d6e014a3",
+      To: 'arslankhan863345@gmail.com',
+      From: "arslankhan863345@gmail.com",
+      Subject: "Email From Website",
+      Body: body
+   }).then(
+      message => {
+         alertTitle.innerText = 'Thank you for reaching me out'
+         alertBody.innerText = "I'll get back to you shortly."
+         alertBox.style.display = 'block';
+         // alert(message);
+         
+         senderName.value = '';
+         email.value = '';
+         message.value = '';
+      }
+
+   ).catch(er =>{
+      alertTitle.innerText = 'Something went wrong'
+         alertBody.innerText = "Try again later.."
+         alertBox.style.display = 'block';
+   });
+})
+
+
+alertCloseBtn.addEventListener('click', (e)=>{
+   e.target.parentNode.style.display = 'none'
+})
+
 
